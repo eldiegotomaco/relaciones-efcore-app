37c039dc65d316d8fe706d67adfcc6d13a942948
eldiegotomaco <iverasooo@gmail.com>
2026-03-31 23:09:57 -0400

    Crud Usuario-Perfil completo con relacion 1:1

--- RelacionesEFCoreApp/Data/ApplicationDbContext.cs
+++ RelacionesEFCoreApp/Data/ApplicationDbContext.cs
@@ -1,13 +1,28 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using RelacionesEFCoreApp.Models;
 
 namespace RelacionesEFCoreApp.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Perfil> Perfiles { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        // 🔥 RELACIÓN 1:1
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Usuario>()
+                .HasOne(u => u.Perfil)
+                .WithOne(p => p.Usuario)
+                .HasForeignKey<Perfil>(p => p.UsuarioId);
         }
     }
 }
---
+++ RelacionesEFCoreApp/Models/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace RelacionesEFCoreApp.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Email { get; set; }
+
+        public Perfil? Perfil { get; set; }
+    }
+}
